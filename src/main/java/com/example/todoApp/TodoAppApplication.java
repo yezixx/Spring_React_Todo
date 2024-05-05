cef627d36bca657e5f80e04ec11cf87e523712b5
yezixx <skylow917@daum.net>
2024-05-05 22:46:43 +0900

    ãhello controller test

--- src/main/java/com/example/todoApp/TodoAppApplication.java
+++ src/main/java/com/example/todoApp/TodoAppApplication.java
@@ -2,6 +2,9 @@ package com.example.todoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoAppApplication {
@@ -10,5 +13,17 @@ public class TodoAppApplication {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
-
+	// Cross-Origin Resource Sharing (CORS)
+	// http://http://localhost:5173/ 으로 부터 오는 모든 요청 허용하기
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // 모든 경로에 대한 HTTP 요청을 처리
+						.allowedMethods("*") // 모든 http 메소드를 허용
+						.allowedOrigins("http://localhost:8080"); // "http://localhost:8080"에서 오는 요청만 허용
+			}
+		};
+	}
 }
