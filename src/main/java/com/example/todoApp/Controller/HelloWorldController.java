29c445ad52790300e0a4d87611355f606d180d03
yezixx <skylow917@daum.net>
2024-05-12 17:16:55 +0900

    hello 클래스/매핑 제거

--- src/main/java/com/example/todoApp/Controller/HelloWorldController.java
+++ src/main/java/com/example/todoApp/Controller/HelloWorldController.java
@@ -24,41 +24,9 @@ public class HelloWorldController {
         return todoData;
     }*/
 
-    @GetMapping("/api/hello")
-    public Hello test() {
-        Hello hello = new Hello("Hello Spring", "Hello React");
-        return hello;
-    }
-
     @GetMapping("/list")
     public List<ToDoEntity> list(){
         List<ToDoEntity> toDoList=this.toDoService.getList();
         return toDoList;
-    }
-
-    public class Hello{
-        private String str;
-        private String str2;
-
-        public Hello(String str, String str2){
-            this.str = str;
-            this.str2=str2;
-        }
-
-        public String getStr() {
-            return str;
-        }
-
-        public void setStr(String str) {
-            this.str = str;
-        }
-
-        public String getStr2() {
-            return str2;
-        }
-
-        public void setStr2(String str2) {
-            this.str2 = str2;
-        }
     }
 }
